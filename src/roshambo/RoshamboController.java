b6703f980a007516234800868b40aed9f56c43fe
Daniel Lester <dlester@stu.jsu.edu>
2021-02-11 15:49:46 -0600

    Standardized presentation

--- src/roshambo/RoshamboController.java
+++ src/roshambo/RoshamboController.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package roshambo;
 
-
-/**
- *
- * @author Daniel
- */
-public class RoshamboController{
+public class RoshamboController {
     
     private int currentMove, totalWins, totalTies, totalLosses;
     
